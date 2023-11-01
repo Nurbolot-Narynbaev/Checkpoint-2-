df1aaa61a294d0e7775a2e0a5677ccd47c0538a7
Nurbolot-Narynbaev <nurbolot.narynbaev@gmail.com>
2023-11-01 15:23:18 +0600

    les

--- Checkpoint.py
+++ Checkpoint.py
@@ -17,10 +17,9 @@
 # seconds= [120,543,666]
 
 # print(convert_seconds_to_hms(seconds))
-
+#
 """task 3"""
 def square_list(lst):
     return list(map(lambda x: x**2, lst))
 
 print(square_list([2, 55, 876]))
-   git
